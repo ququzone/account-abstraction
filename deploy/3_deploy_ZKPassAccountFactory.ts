9975659f93e59003ab88203c546c042fdf180ca3
ququzone <xueping.yang@gmail.com>
2023-03-02 15:37:39 +0800

    add verifier

---
+++ deploy/3_deploy_ZKPassAccountFactory.ts
@@ -0,0 +1,28 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { ethers } from 'hardhat'
+
+const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const provider = ethers.provider
+  const from = await provider.getSigner().getAddress()
+
+  const verifier = await hre.deployments.deploy(
+    'Verifier', {
+      from,
+      args: [],
+      deterministicDeployment: true
+    })
+  console.log('==Verifier addr=', verifier.address)
+
+  // const entrypoint = await hre.deployments.get('EntryPoint')
+  // const ret = await hre.deployments.deploy(
+  //   'SimpleAccountFactory', {
+  //     from,
+  //     args: [entrypoint.address],
+  //     gasLimit: 6e6,
+  //     deterministicDeployment: true
+  //   })
+  // console.log('==SimpleAccountFactory addr=', ret.address)
+}
+
+export default deploySimpleAccountFactory
